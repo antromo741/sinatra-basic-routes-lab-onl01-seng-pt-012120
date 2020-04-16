ef39758774154209dae7393a9fcc3e5518c95629
careful-illustrator-5297 <anthonyroma741@gmail.com>
2020-04-16 16:20:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,6 +3,16 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
   get '/name' do
-    
+    "My name is Anthony"
+  end
+  
+  
+  get '/hometown' do 
+    "My hometown is Middle Island"
+  end
+  
+  get '/favorite-song' do 
+    "My favorite song is Frat Rules"
+  end
   
 end
